e749c1d70afcf6a0188a2867daa2cc062ea91638
lukkiddd <s.phasathorn@gmail.com>
2018-08-19 18:57:26 +0700

    :heavy_plus_sign: Add At-ui

--- activas-client/src/main.js
+++ activas-client/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import AtComponents from 'at-ui'
+import 'at-ui-style'
+
 Vue.config.productionTip = false
+Vue.use(AtComponents)
 
 new Vue({
   render: h => h(App)
